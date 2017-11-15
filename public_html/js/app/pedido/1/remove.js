be6572c0b8587973715429fc575667b77b5cd551
a053603420b <a053603420b@S09-PC02>
2017-11-15 13:41:45 +0100

    remove pedido response data = 0

--- public_html/js/app/pedido/1/remove.js
+++ public_html/js/app/pedido/1/remove.js
@@ -61,7 +61,7 @@ moduloUsuario.controller('PedidoRemove1Controller',
                     serverCallService.remove($scope.ob, $scope.id).then(function (response) {
                         if (response.status == 200) {
                             if (response.data.status == 200) {
-                                if (response.data.json == 1) {
+                                if (response.data.json == 0) {
                                     $scope.status = "El registro con id=" + $scope.id + " se ha eliminado.";
                                 } else {
                                     $scope.status = "Error en el borrado de datos del servidor";
